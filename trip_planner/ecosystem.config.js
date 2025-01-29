4cebf279f44bb4c91f7ac30a0b912b3bee15c9c9
nilpatel <nilpatel62@gmail.com>
2025-01-29 18:38:44 +0530

    ecosystem file change

--- trip_planner/ecosystem.config.js
+++ trip_planner/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [
         {
               name: 'trip-planner',
-              script: 'uvicorn main:app --host 0.0.0.0',
+            script: 'uvicorn main:app --host 0.0.0.0 --port 5009',
               args: '',
               instances: 1,
               autorestart: true,
